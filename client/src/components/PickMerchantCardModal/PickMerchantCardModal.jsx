178b26ff2098fff77989b1a419a32fe2db7362a9
Zach Tang <zachtangoj@gmail.com>
2024-01-13 13:06:02 -0600

    show missing crystals on pick merchant card modal

--- client/src/components/PickMerchantCardModal/PickMerchantCardModal.jsx
+++ client/src/components/PickMerchantCardModal/PickMerchantCardModal.jsx
@@ -1,26 +1,33 @@
 import MerchantCard from '../MerchantCard/MerchantCard'
 import { IoMdClose } from "react-icons/io";
 import './PickMerchantCardModal.css'
-import MerchantCardCrystals from '../MerchantCardCrystals/MerchantCardCrystals';
 import { acquireMove } from '../../clientMessage';
+import Crystal from '../Crystals/Crystal';
 
 
 export default function PickMerchantCardModal({id, position, crystals, onClose}) {
     
     const dropCrystals = [
-        ...new Array(crystals[0]).fill(0),
-        ...new Array(crystals[1]).fill(1),
-        ...new Array(crystals[2]).fill(2),
-        ...new Array(crystals[3]).fill(3),
+        ...new Array(crystals[0]).fill([0, 0]),
+        ...new Array(crystals[1]).fill([1, 0]),
+        ...new Array(crystals[2]).fill([2, 0]),
+        ...new Array(crystals[3]).fill([3, 0])
     ].slice(0, position)
 
     const canAcquire = dropCrystals.length >= position;
+
+    if (!canAcquire) {
+        const numMissingCrystals = position - dropCrystals.length
+        for (let i = 0; i < numMissingCrystals; i++) {
+            dropCrystals.push([0, 1])
+        }
+    }
 
     function onAcquire() {
         if (!canAcquire) {
             return
         }
-        console.log('Acquire ', id, dropCrystals)
+
         acquireMove(id, dropCrystals)
     }
 
@@ -31,7 +38,15 @@ export default function PickMerchantCardModal({id, position, crystals, onClose})
             </div>
             <MerchantCard id={id} />
             <div className="controls">
-                <MerchantCardCrystals crystals={dropCrystals} />
+                <div className='pick-merchant-card-modal-crystals'>
+                    {dropCrystals.map(([crystal, used], i) => (
+                        <Crystal
+                            key={i}
+                            color={crystal}
+                            used={used}
+                        />
+                    ))}
+                </div>
                 <button className={`${!canAcquire ? 'used' : ''}`} onClick={onAcquire}>Acquire</button>
             </div>
         </div>
